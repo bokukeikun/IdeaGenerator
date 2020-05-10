ca4f62627b48e65a426031410c6c412677d0ace5
ichiyasa-g <handwegk02@gmail.com>
2020-05-10 18:20:00 +0900

    second commit

--- IG/urls.py
+++ IG/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='IG-home'),
-    path('category_forms/', views.category_forms, name='category_forms'),
+    path('category_forms/', views.home, name='IG-home'),
+    path('category_forms/get', views.hello_get_query, name='hello_get_query'),
 ]
--- IG/views.py
+++ IG/views.py
@@ -2,46 +2,166 @@ from django.shortcuts import render
 from datetime import datetime
 from . import forms
 
+
 def home(request):
-#     now = datetime.now()
-#     import random
-#     import os
+    import random
+    import os
 
-#     tech = random.choice(Tech_li)
-#     industory = random.choice(Category_li)
-#     feeling = random.choice(Feeling_li)
-#     web = random.choice(Web_service_li)
-#     religion = random.choice(Religion_li)
+    Tech_li = ['AR', 'VR', 'AI', 'IoT', '5G', '6G', '7G', 'CG', 
+            'スマートスピーカー', '3dプリンタ', 'ブロックチェーン', 
+            '自動運転', '言語翻訳']
 
-#     Comb = tech + "×" + industory + "×" + feeling
+    Category_li = ['農業', 'インフラ', 'エネルギー', '医療', '健康',
+                '介護' '自動車', '建築', '金融', '物流', '流通', '防犯', 
+                '防災', '教育',  '飲食', '林業', 'テクノロジー', '感覚']
 
-#     Comb2 = religion + "×" + feeling + "×" + web
+    Agri_li = ['水耕栽培', '地中海式農業', '畜産',  '酪農', '園芸農業', 
+            '混合農業', 'プランテーション', '遊牧', '焼畑', '耕作', 
+            '有機栽培', '自然農法', '無農薬', 'バイオダイナミック農法', 
+            '植物工場']
 
-#     context = {
-#         'tech': tech,
-#         'industory': industory,
-#         'religion': religion,
-#         'web': web,
-#         'feeling': feeling,
-#         'text': Comb,
-#     }
-    return render(request, 'IG/home.html')
+    Web_service_li = ['情報検索機能', '情報自動所得', 'EC', 'web会議', 
+                    'データベース', 'クラウド', 'SNS', 'メディア']
 
-# def category_forms(request):
-#     form = forms.CategoryForm()
-#     d = {
-#         'form': form
-#     }
-#     return render(request, 'IG/form.html', d)
+    AI_li = ['迷惑メールフィルタ', '音声認識', 'レコメンデーション', '会話bot', 
+            '音声合成', '自動運転車', '感情推定', '画像認識', '画像変換']
 
-def category_forms(request):
-    form = forms.CategoryForm(request.GET or None)
-    if form.is_valid():
-        message = 'データ検証に成功しました'
-    else:
-        message = 'データ検証に失敗しました'
+    Religion_li = ['ユダヤ教', 'キリスト教系', 'キリスト教', 'キリスト教系新宗教', 
+                'イスラーム', 'インド宗教', 'ヒンドゥー教', 'インドの自由思想家', 
+                'ジャイナ教', 'シク教', 'ゾロアスター教', '仏教', 'インド仏教', 
+                '南伝仏教', '北伝仏教', '西アジアの宗教', '中央アジアの宗教', 
+                '東アジアの宗教', '精神修養', '土着宗教など', 'ネオペイガニズム', 
+                'アフロ・クレオール宗教', 'サタニズム系', '秘教', '神秘主義', 
+                    'パロディー宗教', '非宗教、反宗教、メタ宗教']
+
+    Feeling_li = ['触覚', '味覚', '嗅覚', '視覚', '聴覚']
+
+
+    Tech_li = {'tech0': 'AR', 'tech1': 'VR', 'tech2': 'AI', 'tech3': 'IoT', 'tech4': '5G', 'tech5': '6G', 'tech6': '7G', 'tech7': 'CG', 'tech8': 'スマートスピーカー', 'tech9': '3dプリンタ', 'tech10': 'ブロックチェーン', 'tech11': '自動運転', 'tech12': '言語翻訳'}
+    Agri_li = {'agri0': '水耕栽培', 'agri1': '地中海式農業', 'agri2': '畜産', 'agri3': '酪農', 'agri4': '園芸農業', 'agri5': '混合農業', 'agri6': 'プランテーション', 'agri7': '遊牧', 'agri8': '焼畑', 'agri9': '耕作', 'agri10': '有機栽培', 'agri11': '自然農法', 'agri12': '無農薬', 'agri13': 'バイオダイナミック農法', 'agri14': '植物工場'}
+    Web_service_li = {'web0': '情報検索機能', 'web1': '情報自動所得', 'web2': 'EC', 'web3': 'web会議', 'web4': 'データベース', 'web5': 'クラウド', 'web6': 'SNS', 'web7': 'メディア'}
+    AI_li = {'AI0': '迷惑メールフィルタ', 'AI1': '音声認識', 'AI2': 'レコメンデーション', 'AI3': '会話bot', 'AI4': '音声合成', 'AI5': '自動運転車', 'AI6': '感情推定', 'AI7': '画像認識', 'AI8': '画像変換'}
+    Religion_li = {'religion0': 'ユダヤ教', 'religion1': 'キリスト教系', 'religion2': 'キリスト教', 'religion3': 'キリスト教系新宗教', 'religion4': 'イスラーム', 'religion5': 'インド宗教', 'religion6': 'ヒンドゥー教', 'religion7': 'インドの自由思想家', 'religion8': 'ジャイナ教', 'religion9': 'シク教', 'religion10': 'ゾロアスター教', 'religion11': '仏教', 'religion12': 'インド仏教', 'religion13': '南伝仏教', 'religion14': '北伝仏教'}
+    Feeling_li = {'feeling0': '触覚', 'feeling1': '味覚', 'feeling2': '嗅覚', 'feeling3': '視覚', 'feeling4': '聴覚'}
+    a = request.GET.get('test1')
+    b = request.GET.get('test2')
+    c = request.GET.get('test3')
+    a_out = None
+    b_out = None
+    c_out = None
+    Comb = None
+
+    categories = {'tech': Tech_li, 'agri': Agri_li, 'feeling': Feeling_li,
+     'web':Web_service_li, 'AI': AI_li, 'religion': Religion_li}
+
+    for category, value in categories.items():
+        if (a != b and a != c and b != c):
+            if (category == a):
+                a_out = random.choice(list(value.values()))
+            elif (category == b):
+                b_out = random.choice(list(value.values()))
+            elif (category == c):
+                c_out = random.choice(list(value.values()))
+        elif (a == b and a == c and b == c ):
+            if (category == a):
+                a_out = random.choice(list(value.values()))
+                b_out = random.choice(list(value.values()))
+                c_out = random.choice(list(value.values()))
+        elif (a == b and a != c):
+            if (category == a):
+                a_out = random.choice(list(value.values()))
+                b_out = random.choice(list(value.values()))
+            elif (category == c):
+                c_out = random.choice(list(value.values()))
+        elif (a == c and a != b):
+            if (category == a):
+                a_out = random.choice(list(value.values()))
+                c_out = random.choice(list(value.values()))
+            elif (category == b):
+                b_out = random.choice(list(value.values()))
+        elif (b == c and a != b):
+            if (category == b):
+                b_out = random.choice(list(value.values()))
+                c_out = random.choice(list(value.values()))
+            elif (category == a):
+                a_out = random.choice(list(value.values()))
+
+
+    for i in range(len(Tech_li)):
+        for key, value in Tech_li.items():
+            if key == a:
+                a_out = value
+            elif key == b:
+                b_out = value
+            elif key == c:
+                c_out = value
+
+    for i in range(len(Agri_li)):
+        for key, value in Agri_li.items():
+            if key == a:
+                a_out = value
+            elif key == b:
+                b_out = value
+            elif key == c:
+                c_out = value
+
+    for i in range(len(Web_service_li)):
+        for key, value in Web_service_li.items():
+            if key == a:
+                a_out = value
+            elif key == b:
+                b_out = value
+            elif key == c:
+                c_out = value
+
+    for i in range(len(AI_li)):
+        for key, value in AI_li.items():
+            if key == a:
+                a_out = value
+            elif key == b:
+                b_out = value
+            elif key == c:
+                c_out = value
+
+    for i in range(len(Religion_li)):
+        for key, value in Religion_li.items():
+            if key == a:
+                a_out = value
+            elif key == b:
+                b_out = value
+            elif key == c:
+                c_out = value
+
+    for i in range(len(Feeling_li)):
+        for key, value in Feeling_li.items():
+            if key == a:
+                a_out = value
+            elif key == b:
+                b_out = value
+            elif key == c:
+                c_out = value
+    
+    if (a_out is not None) and (b_out is not None) and (c_out is not None):
+        Comb = a_out + "　×　" + b_out + "　×　" + c_out  
+
+    context = {
+        'tech': Tech_li,
+        'agri': Agri_li,
+        'web': Web_service_li,
+        'AI': AI_li,
+        'religion': Religion_li,
+        'feeling': Feeling_li,
+        'a': a_out,
+        'b': b_out,
+        'c': c_out,
+        'text': Comb,
+    }
+    return render(request, 'IG/form.html', context)
+
+def hello_get_query(request):
     d = {
-        'form': form,
-        'message': message,
+        'test1':request.GET.get('test1'),
+        'test2':request.GET.get('test2'),
+        'test3':request.GET.get('test3'),
     }
     return render(request, 'IG/form.html', d)
