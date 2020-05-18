36d945b4c726794558f3309fadbd4990b616145c
ichiyasa-g <handwegk02@gmail.com>
2020-05-18 10:15:52 +0900

    change ig.js

--- static/js/ig.js
+++ static/js/ig.js
@@ -51,7 +51,7 @@
     
 
 
-   var Categories_ja = {'phisi': '物理', 'medi': '医学', 'chemi': '化学', 'tech1_': 'テクノロジー１', 'tech_2': 'テクノロジー２', 'energy': 'エネルギー', 'nature': '自然', 'agri': '農業', 'space': '宇宙', 'buisiness': 'ビジネス', 'infra': 'インフラ', 'poli': '政治', 'nation': '国', 'inst1_': '施設１', 'inst2_': '施設２', 'edu': '教育', 'cust': '習慣', 'art': '芸術', 'life': '生活・暮らし', 'sense': '五感', 'feeling': '感情', 'bp': '体のパーツ'}
+   var Categories_ja = {'phisi': '物理', 'medi': '医学', 'chemi': '化学', 'tech1_': 'テクノロジー１', 'tech_2': 'テクノロジー２', 'energy': 'エネルギー', 'nature': '自然', 'agri': '農業', 'space': '宇宙', 'buisiness': 'ビジネス', 'infra': 'インフラ', 'poli': '政治', 'nation': '国', 'inst1_': '施設１', 'inst2_': '施設２', 'edu': '教育', 'cust': '習慣', 'art': '芸術', 'life': '生活・暮らし', 'sense': '五感', 'feeling': '感情', 'bp': '体のパーツ'};
 
 
    var category1_get = arg.test1;
@@ -111,6 +111,7 @@
     });
         
     $('.idea_generated').on('click', function(){
+        // $("select option").attr("selected", false);
         var tag1 = $(this).find('.idea1').text();
         var tag2 = $(this).find('.idea2').text();
         var tag3 = $(this).find('.idea3').text();
