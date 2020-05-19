b74c1ba1d8db35619d9d1f7118ef7798cc1bf6bb
Ryo <ryo91.hb@gmail.com>
2020-05-19 23:18:16 +0900

    ryo heroku modify 6

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'config.urls'
